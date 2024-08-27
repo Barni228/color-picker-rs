1e6cb869c349595a25f51063e06830a626d40eeb
Andrii Pukhalyk <pukhalyk1@gmail.com>
2024-08-27 15:52:36 -0600

    Added input field that asks for hex code

--- color-picker-rust-fltk/src/main.rs
+++ color-picker-rust-fltk/src/main.rs
@@ -1,9 +1,36 @@
-use fltk::{app, prelude::*, window::Window};
+use fltk::{app, enums, frame, input, prelude::*, window::Window};
 
 fn main() {
     let app = app::App::default();
-    let mut wind = Window::new(0, 0, 400, 400, "Color picker").center_screen();
-    wind.end();
-    wind.show();
+
+    // create window at the center of the screen
+    let mut win = Window::new(0, 0, 400, 400, "Color picker").center_screen();
+
+    // box with color preview
+    let mut color_preview = frame::Frame::new(100, 30, 200, 50, "");
+
+    color_preview.set_color(enums::Color::Red);
+    color_preview.set_frame(enums::FrameType::FlatBox); // Set the frame type to fill the background
+
+    let mut hex_field = input::Input::new(100, 150, 200, 30, "Hex:");
+    hex_field.set_maximum_size(7);
+    hex_field.handle(move |field, ev| {
+        if ev == enums::Event::KeyDown {
+            match app::event_key() {
+                enums::Key::Enter => {
+                    let color = enums::Color::from_hex_str(&field.value()).unwrap();
+                    color_preview.set_color(color);
+                    color_preview.redraw();
+                    true
+                }
+                _ => false,
+            }
+        } else {
+            false
+        }
+    });
+
+    win.end();
+    win.show();
     app.run().unwrap();
 }
